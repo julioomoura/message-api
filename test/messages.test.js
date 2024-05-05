96108dcf9a0da2585a56e77c261b818708ec16c3
Júlio <juliomourajom@gmail.com>
2024-05-05 16:50:34 -0300

    Add missing tests

--- test/messages.test.js
+++ test/messages.test.js
@@ -2,9 +2,11 @@ import { describe, it, mock, afterEach } from "node:test";
 import assert from "node:assert";
 import MessageService from "../src/message.service.js";
 
-describe("getMessage", () => {
+describe("Message Service", () => {
     const redisClient = {
         GET: mock.fn(),
+        SET: mock.fn(),
+        DEL: mock.fn(),
     };
     const randomCommitApi = {
         getRandomCommitMessage: mock.fn(),
@@ -14,21 +16,40 @@ describe("getMessage", () => {
     afterEach(() => {
         mock.reset();
     });
+    describe("getMessage", () => {
+        it("should return message from cache when it is cached", async () => {
+            const cachedMessage = "Hello, my friend";
 
-    it("should return message from cache when it is cached", async () => {
-        const cachedMessage = "Hello, my friend";
+            mock.method(redisClient, "GET", async () =>
+                Promise.resolve(cachedMessage),
+            );
 
-        mock.method(redisClient, "GET", async () => Promise.resolve(cachedMessage));
-
-        const teste = await service.getMessage();
-        assert.equal(teste, cachedMessage);
+            const message = await service.getMessage();
+            assert.equal(message, cachedMessage);
+        });
+        it("should return message from api when it is not cached", async () => {
+            const apiMessage = "Random commit message";
+            mock.method(randomCommitApi, "getRandomCommitMessage", async () =>
+                Promise.resolve(apiMessage),
+            );
+            const message = await service.getMessage();
+            assert.equal(message, apiMessage);
+        });
     });
-    it("should return message from api when it is not cached", async () => {
-        const apiMessage = "Random commit message";
-        mock.method(randomCommitApi, "getRandomCommitMessage", async () =>
-            Promise.resolve(apiMessage),
-        );
-        const teste = await service.getMessage();
-        assert.equal(teste, apiMessage);
-    });
+
+    describe("cacheMessage", () => {
+        it("should cache the given message", async () => {
+            const message = "message"
+            await service.cacheMessage(message)
+            assert.deepStrictEqual(redisClient.SET.mock.calls[0].arguments, ["message", message, { EX: 60 }])
+        })
+    })
+
+    describe("cacheMessage", () => {
+        it("should flush the cache", async () => {
+            const message = "message"
+            await service.flushCache()
+            assert.deepStrictEqual(redisClient.DEL.mock.calls[0].arguments, ["message"])
+        })
+    })
 });
